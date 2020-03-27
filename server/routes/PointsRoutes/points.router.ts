72c682ca4743ac946db56ff64ebea0c1055c10a0
Allen Lucke <allenlucke@gmail.com>
2020-03-27 12:49:41 -0500

    modified points router querreis to return 0 values insted of nulls for total blocks and colors when none had been added

--- server/routes/PointsRoutes/points.router.ts
+++ server/routes/PointsRoutes/points.router.ts
@@ -9,27 +9,30 @@ const router: express.Router = express.Router();
 //GET route for current points, level, and blocks added
 router.get('/:userId', rejectUnauthenticated, (req: Request, res: Response, next: express.NextFunction): void => {
     const userId: number | null = <number>parseInt(req.params.userId);
-    const queryText: string = `SELECT "user".points AS "points", "user".user_levels AS "user_levels", 
-                                COUNT("blocks") AS "totalBlocks" FROM "user"
-                                JOIN "levels" ON "user".user_levels = "levels".id
-                                JOIN "blocks" ON "user".id = "blocks".user_id
-                                WHERE "user".id = $1
-                                GROUP BY "user".points, "user".user_levels;`;
+    const queryText: string = `SELECT 
+    coalesce("user".points, 0) AS "points",
+    coalesce("user".user_levels, 0) AS "user_levels"
+    FROM "user"
+    WHERE "user".id = $1
+    GROUP BY "user".points, "user".user_levels;`;
     pool.query(queryText, [userId])
     .then((response1) => {
-        // console.log(response1.rows[0])
+        console.log(response1.rows[0])
         const pointsArray: any = response1.rows.map((item, index) => {
             return <number>item
         })
-        // console.log(pointsArray[0].points)
-        // console.log(pointsArray[0].user_levels)
+        console.log(pointsArray[0].points)
+        console.log(pointsArray[0].user_levels)
         const currentPoints: number = parseInt(pointsArray[0].points)
         const userLvlId: number = parseInt(pointsArray[0].user_levels)
         // console.log(userLvlId)
         // console.log(currentPoints)
-        //GET route for colors added
-        const queryText = `SELECT COUNT("colors_user") AS "totalColors" FROM "colors_user"
+        //GET route for total colors and blocks added
+        const queryText = `SELECT COUNT("colors_user") AS "totalColors",
+                            coalesce(COUNT("blocks".id), 0) AS "totalBlocks"
+                            FROM "colors_user"
                             JOIN "user" ON "colors_user".user_id = "user".id
+                            JOIN "blocks" ON "user".id = "blocks".user_id
                             WHERE "user".id = $1;`;
         pool.query(queryText, [userId])
         .then((response2) => {
